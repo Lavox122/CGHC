8265a852fabbf9c5fb9c50f5b8f80b134d7d2f73
Lavox122 <Jinsheng22222@gmail.com>
2025-02-24 14:42:33 +0800

    Fixed platform
    still buggy

--- Assets/Scripts/Level Components/MovingPlatform.cs
+++ Assets/Scripts/Level Components/MovingPlatform.cs
@@ -8,13 +8,11 @@ public class MovingPlatform : PathFollow
     public bool CollidingWithPlayer { get; set; }
     public bool loop = true; //to control between looping platform and stopping at the end
     public bool Activated = true; //set to false to make it activate only when player is on it
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<PlayerController>() != null)
-        {
-            CollidingWithPlayer = true;
-            Activated = true;
-        }
+        CollidingWithPlayer = true;
+        Activated = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
@@ -25,13 +23,9 @@ public class MovingPlatform : PathFollow
     protected override void Update()
     {
         if (!Activated && !loop) return;
+        if(!loop && ReachedEnd()) return;
 
         base.Update();
-
-        if(!loop && ReachedEnd())
-        {
-            enabled = false;
-        }
     }
 
     private bool ReachedEnd()
