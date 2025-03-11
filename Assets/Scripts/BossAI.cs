6d7341831310dc0a22e56b76a3b4e15c56f18e64
jinsheng22222@gmail.com <jinsheng22222@gmail.com>
2025-03-11 22:29:07 +0800

    lost my progress, unity crashed

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -8,6 +8,10 @@ public class BossAI : MonoBehaviour
     private bool canAct = true;
     public int health = 30;
 
+    public Transform PointA;
+    public Transform PointB;
+    private bool moving;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,27 +27,52 @@ public class BossAI : MonoBehaviour
 
             canAct = false;
 
-            int action = Random.Range(0, 3);
+            int action = Random.Range(0, 2);
             if (action == 0)
             {
                 animator.SetTrigger("Jump");
+
+                yield return new WaitForSeconds(GetAnimationLength("Jump"));
+
+                yield return StartCoroutine(MoveBetweenPoints());
+
+                animator.SetTrigger("Drop");
+                yield return new WaitForSeconds(GetAnimationLength("Drop"));
             }
             else
             {
                 animator.SetTrigger("Attack");
             }
 
-            yield return new WaitForSeconds(GetAnimationLength());
-
             yield return new WaitForSeconds(4f);
             canAct = true;
         }
     }
 
-    private float GetAnimationLength()
+    private float GetAnimationLength(string animationName)
     {
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        AnimatorStateInfo stateInfo;
+
+        do
+        {
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        } while (!stateInfo.IsName(animationName));
+
         return stateInfo.length;
+    }
+
+    IEnumerator MoveBetweenPoints()
+    {
+        Transform target = moving ? PointA : PointB;
+        moving = !moving;
+
+        float moveSpeed = 100f;
+
+        while (Vector2.Distance(transform.position, target.position) > 0.1f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 
     private void TakeDamage(Collider2D objectCollided)
