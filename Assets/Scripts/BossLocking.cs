54d54c94d0afd3076cf3ce1c92f709097db09871
jinsheng22222@gmail.com <jinsheng22222@gmail.com>
2025-03-16 15:44:29 +0800

    Level 4 Done

--- Assets/Scripts/BossLocking.cs
+++ Assets/Scripts/BossLocking.cs
@@ -1,21 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BossLocking : MonoBehaviour
 {
-
+    public GameObject Self;
     public GameObject Gate;
     public GameObject Boss;
-    // Start is called before the first frame update
+    public GameObject FakeBoss;
+    public TMP_Text bossNameText;
+    public TMP_Text warningText;
+
+    public float textDisplayDuration = 3f;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Gate.SetActive(true);
-            Boss.SetActive(true);
-            this.enabled = false;
+            StartCoroutine(ShowBossName());
         }
+    }
+
+    IEnumerator ShowBossName()
+    {
+        bossNameText.gameObject.SetActive(true);
+        warningText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(textDisplayDuration);
+
+        bossNameText.gameObject.SetActive(false);
+        warningText.gameObject.SetActive(false);
+
+        FakeBoss.SetActive(false);
+        Boss.SetActive(true);
+
+        Self.SetActive(false);
     }
 }
