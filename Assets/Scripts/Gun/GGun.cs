42f9db16d8415f6172a44926eeb896c3b39dd90c
Ganyu-LOVER <jinsheng22222@gmail.com>
2026-01-13 12:45:52 +0800

    Bunch of Updates

---
+++ Assets/Scripts/Gun/GGun.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private Animator modelAnimator;
+
+    [Header("Gun Settings")]
+    [SerializeField] private float msBetweenShots = 100;
+
+    [Header("Ammo")]
+    [SerializeField] private int magazineSize = 30;
+    [SerializeField] private bool autoReload = true;
+    [SerializeField] private float reloadTime = 3f;
+
+    public GameObject MagazinePrefab;
+    public Transform dropPoint;
+
+    public GameObject ReloadingModel;
+    public GameObject Model;
+
+    // Reference to GunController
+    public GunController GunController { get; set; }
+
+    private ObjectPooler _pooler;
+    private float _nextShotTime;
+    private bool _isReloading;
+    private int _projectilesRemaining;
+
+    private int _fireParameter = Animator.StringToHash("Fire");
+
+    private void Start()
+    {
+        _pooler = GetComponent<ObjectPooler>();
+        _projectilesRemaining = magazineSize;
+    }
+
+    private void Update()
+    {
+        if (autoReload)
+        {
+            Reload(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload(false);
+        }
+    }
+
+    // Fires a projectile from the firePoint
+    private void FireProjectile()
+    {
+        GameObject newProjectile = _pooler.GetObjectFromPool();
+        newProjectile.transform.position = firePoint.position;
+        newProjectile.SetActive(true);
+
+        Projectile projectile = newProjectile.GetComponent<Projectile>();
+        projectile.GunEquipped = this;
+        projectile.SetDirection(GunController.PlayerController.FacingRight ? Vector3.right : Vector3.left);
+        projectile.EnableProjectile();
+
+        modelAnimator.SetTrigger(_fireParameter);
+    }
+
+    // Shoots the Gun
+    public void Shoot()
+    {
+        if (Time.time > _nextShotTime && !_isReloading && _projectilesRemaining > 0)
+        {
+            _nextShotTime = Time.time + msBetweenShots / 1000f;
+            FireProjectile();
+            _projectilesRemaining--;
+
+            SoundManager.Instance.PlaySound(AudioLibrary.Instance.GunShootClip);
+        }
+    }
+
+    // Reloads the gun
+    public void Reload(bool autoReload)
+    {
+        if (_projectilesRemaining > 0 && _projectilesRemaining <= magazineSize && !_isReloading && !autoReload)
+        {
+            StartCoroutine(IEWaitForReload());
+        }
+
+        if (_projectilesRemaining <= 0 && !_isReloading)
+        {
+            StartCoroutine(IEWaitForReload());
+        }
+    }
+
+
+    // Reload coroutine
+    private IEnumerator IEWaitForReload()
+    {
+        _isReloading = true;
+        ReloadingModel.SetActive(true);
+        Model.SetActive(false);
+
+        Instantiate(MagazinePrefab, dropPoint.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(reloadTime);
+
+        Model.SetActive(true);
+        ReloadingModel.SetActive(false);
+
+        _projectilesRemaining = magazineSize;
+        _isReloading = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStates/PlayerJetpack1 N.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerJetpack : PlayerStates
+{
+    [Header("Settings")]
+    [SerializeField] private float jetpackForce = 10f;
+    [SerializeField] private float jetpackFuel = 5f;
+    [SerializeField] private float jetpackPitch = 1f;
+
+    public float JetpackFuel { get; set; }
+    public float FuelLeft { get; set; }
+    public float InitialFuel => jetpackFuel;
+
+    private float _fuelLeft;
+    private float _fuelDuractionLeft;
+    private bool _stillHaveFuel = true;
+
+    private int _jetpackParameter = Animator.StringToHash("Jetpack");
+
+    // Declare the AudioSource for the looping jetpack sound
+    private AudioSource _jetpackAudioSource;
+
+    protected override void InitState()
+    {
+        base.InitState();
+        JetpackFuel = jetpackFuel;
+        _fuelDuractionLeft = JetpackFuel;
+        FuelLeft = JetpackFuel;
+        UIManager.Instance.UpdateFuel(FuelLeft, JetpackFuel);
+    }
+
+    protected override void GetInput()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            Jetpack();
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            EndJetpack();
+        }
+    }
+
+    private void Jetpack()
+    {
+        if (!_stillHaveFuel)
+        {
+            return;
+        }
+
+        if (FuelLeft <= 0)
+        {
+            EndJetpack();
+            _stillHaveFuel = false;
+            return;
+        }
+
+        // If not already jetpacking, start playing the looping jetpack sound
+        if (!_playerController.Conditions.IsJetpacking)
+        {
+            if (_jetpackAudioSource == null)
+            {
+                // Create an AudioSource component for the jetpack sound
+                _jetpackAudioSource = gameObject.AddComponent<AudioSource>();
+                _jetpackAudioSource.clip = AudioLibrary.Instance.JetpackClip;
+                _jetpackAudioSource.loop = true;
+                _jetpackAudioSource.pitch = jetpackPitch;
+            }
+            if (!_jetpackAudioSource.isPlaying)
+            {
+                _jetpackAudioSource.Play();
+            }
+        }
+
+        _playerController.SetVerticalForce(jetpackForce);
+        _playerController.Conditions.IsJetpacking = true;
+        StartCoroutine(BurnFuel());
+    }
+
+    private void EndJetpack()
+    {
+        _playerController.Conditions.IsJetpacking = false;
+
+        // Immediately stop and remove the looping jetpack audio if it's playing,
+        // ensuring an instant cutoff of the sound.
+        if (_jetpackAudioSource != null)
+        {
+            _jetpackAudioSource.Stop();
+            Destroy(_jetpackAudioSource);
+            _jetpackAudioSource = null;
+        }
+
+        StartCoroutine(Refill());
+    }
+
+    private IEnumerator BurnFuel()
+    {
+        float fuelConsumed = FuelLeft;
+        if (fuelConsumed > 0 && _playerController.Conditions.IsJetpacking && FuelLeft <= fuelConsumed)
+        {
+            fuelConsumed -= Time.deltaTime;
+            FuelLeft = fuelConsumed;
+            UIManager.Instance.UpdateFuel(FuelLeft, JetpackFuel);
+            yield return null;
+        }
+    }
+
+    private IEnumerator Refill()
+    {
+        yield return new WaitForSeconds(5f);
+        float fuel = FuelLeft;
+        while (fuel < JetpackFuel && !_playerController.Conditions.IsJetpacking)
+        {
+            fuel += Time.deltaTime;
+            FuelLeft = fuel;
+            UIManager.Instance.UpdateFuel(FuelLeft, JetpackFuel);
+
+            if (!_stillHaveFuel && fuel > 0.2f)
+            {
+                _stillHaveFuel = true;
+            }
+
+            yield return null;
+        }
+    }
+
+    public override void SetAnimation()
+    {
+        _animator.SetBool(_jetpackParameter, _playerController.Conditions.IsJetpacking);
+    }
+}
